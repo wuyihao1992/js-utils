02a2c1d232d7d62365d29742dfcf4e1521cc6dca
wuyihao <wuyihao@xiao100.com>
2020-01-14 15:22:13 +0800

    feat: [v1.0] update element

--- element/index.js
+++ element/index.js
@@ -10,3 +10,33 @@ export function preventDefault(e) {
     e.stopPropagation();
     e.preventDefault();
 }
+
+/**
+ * 判断内容有无超出宽度
+ * @param el
+ * @return {boolean}
+ */
+export function hasOverflowX(el) {
+    if (!el) {
+        return false;
+    }
+
+    return el.scrollLeft > el.clientWidth || el.offsetWidth > el.clientWidth;
+}
+
+/**
+ * 计算最大左右滚动距离
+ * @param el
+ */
+export function computeMaxScrollLeft(el) {
+    if (!el) {
+        return 0;
+    }
+
+    let maxLeft = 0;
+    if (hasOverflowX(el)) {
+        maxLeft = el.scrollWidth - el.clientWidth;
+    }
+
+    return maxLeft;
+}
